8b63c2de00a63ffa0fb20a5474f671cb7c8495f2
Zack Dizdar <zdizdar@macalester.edu>
2018-10-03 19:33:33 -0500

    Got rid of contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { ShoppingList } from '../pages/shopping_list/shopping_list';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -15,7 +15,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    ShoppingList,
     HomePage,
     TabsPage
   ],
@@ -27,7 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    ShoppingList,
     HomePage,
     TabsPage
   ],
---
+++ src/pages/shopping_list/shopping_list.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-shoppinglist',
+  templateUrl: 'shopping_list.html'
+})
+export class ShoppingList {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
